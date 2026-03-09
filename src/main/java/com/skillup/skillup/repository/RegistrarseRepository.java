f263390832425adcb54a4333dd39d712643e4da4
DIEGO <diegotapia.s@hotmail.com>
2026-03-09 15:11:01 -0500

    fix: ajustar tipo de ID en RegistrarseRepository para persistencia correcta

--- src/main/java/com/skillup/skillup/repository/RegistrarseRepository.java
+++ src/main/java/com/skillup/skillup/repository/RegistrarseRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RegistrarseRepository extends JpaRepository<Registrarse, Long> {
+public interface RegistrarseRepository extends JpaRepository<Registrarse, String> {
 
     boolean existsByIdentificacion(String identificacion);
 
