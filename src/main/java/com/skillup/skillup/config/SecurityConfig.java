16fc26f1e253eaa7faddbeacf8dbb452c629421d
DIEGO <diegotapia.s@hotmail.com>
2026-03-11 15:11:00 -0500

    Corrección: Arreglar error de sintaxis y falta de importación en SecurityConfig.java

--- src/main/java/com/skillup/skillup/config/SecurityConfig.java
+++ src/main/java/com/skillup/skillup/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.skillup.skillup.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -20,6 +21,8 @@ public class SecurityConfig {
 
     @Bean
     public UserDetailsService userDetailsService() {
+        return new CustomUserDetailsService();
+    }
 
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
