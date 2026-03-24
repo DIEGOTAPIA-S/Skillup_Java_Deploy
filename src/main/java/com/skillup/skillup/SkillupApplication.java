fd9a37ca6fb8bf4d40067053fcea00e030375143
DIEGO <diegotapia.s@hotmail.com>
2026-03-24 14:10:43 -0500

    Fix: cambiado puerto de correo a 465 (SSL) para evitar bloqueos de conexión en Render

--- src/main/java/com/skillup/skillup/SkillupApplication.java
+++ src/main/java/com/skillup/skillup/SkillupApplication.java
@@ -2,13 +2,14 @@ package com.skillup.skillup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class SkillupApplication {
 
 	public static void main(String[] args) {
-
-        SpringApplication.run(SkillupApplication.class, args);
+		SpringApplication.run(SkillupApplication.class, args);
 	}
 
 }
